0bfe5e2c13fdf9464af42bc5ddbd7450d705823a
Alan Somers <asomers@gmail.com>
2022-11-07 10:40:53 -0700

    `enter` and `get_mode` now return a `io::Result`

--- src/process.rs
+++ src/process.rs
@@ -2,9 +2,11 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-pub fn enter() -> Result<(), ()> {
+use std::io;
+
+pub fn enter() -> io::Result<()> {
     if unsafe { cap_enter() } < 0 {
-        Err(())
+        Err(io::Error::last_os_error())
     } else {
         Ok(())
     }
@@ -14,11 +16,11 @@ pub fn sandboxed() -> bool {
     unsafe { cap_sandboxed() == 1 }
 }
 
-pub fn get_mode() -> Result<usize, ()> {
+pub fn get_mode() -> io::Result<usize> {
     let mut mode = 0;
     unsafe {
         if cap_getmode(&mut mode as *mut usize) != 0 {
-            return Err(());
+            return Err(io::Error::last_os_error());
         }
     }
     Ok(mode)
